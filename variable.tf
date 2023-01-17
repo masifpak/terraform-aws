805c96e3220474fe55f468f3e1a606b28f5d3378
masifeddress <90234516+masifeddress@users.noreply.github.com>
2023-01-17 18:39:13 +0500

    Update variable

--- variable.tf
+++ variable.tf
@@ -7,5 +7,5 @@ variable "profile" {
   description = "AWS Profile."
   type = string
 }
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+variable "aws_access_key" {}
+variable "aws_secret_access_key" {}
